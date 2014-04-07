bbec76a653c3e921d66ab5b1656c7817ecc6f037
twistedpixel <scott@pixelbendstudios.com>
2014-04-08 00:12:59 +0100

    Oops, spark.js should really be in the root of the repo

---
+++ js/spark.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+
+	// Toggle menu visibility (height) on mobile devices
+	var nav = false
+	var selector = 'nav#primary-nav .nav-list > li'
+	
+	$('.nav-link').on('click', function() {
+		var height = 0
+		$(selector).each(function(){
+			height += parseInt($(this).css('height'))
+		})
+
+		if(nav == false) {
+			$('.nav-list').css({"height": height})
+			nav = true
+		}
+		else {
+			$('.nav-list').css({"height": "0"})
+			nav = false
+		}
+		return false;
+	})
+	
+	// Accessibility fix for navigation menu dropdowns
+	$(".dd-container a").on('focus', function() {
+		$(this).parent('li.dd-container').addClass('open')
+	}).on('blur', function() {
+		$(this).parent('li.dd.container').removeClass('open')
+	})
+	
+	// Close button for alerts
+	$('.alert .alert-close').on('click', function() {
+		$(this).parent().remove();
+		return false;
+	})
+	
+	// Modal open trigger
+	$('.modal-trigger:not(.modal-static)').on('click', function() {
+		modal = $('#' + $(this).data('modal-trigger'));
+		modal.css({'display':'inherit'});
+		$('#blackout').css({'display':'block'});
+		setTimeout(function() {
+			modal.addClass('modal-show'); $('#blackout').addClass('show');
+		}, 100);
+		return false;
+	});
+	
+	// Modal close trigger
+	$('.modal-close-trigger').on('click', function() {
+		modal = $('#' + $(this).data('modal-trigger'));
+		modal.removeClass('modal-show');
+		$('#blackout').removeClass('show');
+		setTimeout(function() {
+			$('#blackout').css({'display':'none'});
+			modal.css({'display':'none'})
+		}, 400);
+		return false;
+	});
+
+});
